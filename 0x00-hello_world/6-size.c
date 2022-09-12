d1158e66c92a93d7d8657b7c362c2f5dfd55274d
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-11 20:34:32 -0700

    COMMIT TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /*
  * main - prints all the prints the size of various types on the computer it is compiled and run on.
  *
- * Description: C program that prints the size of various types on the computer it is compiled and run on
+ * Description: C program that prints the size of various types
+ * on the computer it is compiled and run on
  *
  * Return: 0
  *
@@ -18,11 +19,11 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of a int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
  
