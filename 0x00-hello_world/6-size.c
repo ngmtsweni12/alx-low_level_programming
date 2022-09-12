6f88666c8d5e24afd3ee82ddb0b241a4edf245cd
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-11 21:12:43 -0700

    COMMIT TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,14 @@
  * Return: 0
  *
  */
-int main(void) 
+int main(void)
 {
 	char c;
 	int i;
 	long li;
 	long long lli;
 	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
