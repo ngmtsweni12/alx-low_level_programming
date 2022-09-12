80493fedcbd1933d0c6e7a5b1036d539665796ae
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-11 20:25:23 -0700

    COMMIT TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,18 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char c));
-	printf("Size of a int: %d byte(s)\n", sizeof(int i));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long li));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long lli));
-	printf("Size of a float: %d byte(s)\n", sizeof(float f));
+	/* Declarations. */
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
  
