8aa95c7269fa81ad94de0a88558a78dd20ccf99e
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-11 18:49:50 -0700

    COMMIT TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-/* 
+/** 
  * This is a C program that prints exactly the following message.
  * Uses the function puts not printf
  * The program ends with the value 0
  */
-
 int main(void)
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
