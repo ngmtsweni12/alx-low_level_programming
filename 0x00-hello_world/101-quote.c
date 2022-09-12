16f77c128af9d55b174a7f33ce913804b37366d9
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-12 00:06:41 -0700

    COMMIT ADVANCED TASK 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print without the use of printf
+ *
+ * Description: a C program that prints exactly
+ * and that piece of art is useful
+ * - Dora Korpar, 2015-10-19, 
+ * followed by a new line, to the standard error.
+ *
+ * Return: 1
+ *
+ */
+
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" 
+			- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
