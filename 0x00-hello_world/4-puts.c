49e162375d6ebcc1879b0f7abe653d3047fb48e3
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-11 18:46:39 -0700

    COMMIT TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/* 
+ * This is a C program that prints exactly the following message.
+ * Uses the function puts not printf
+ * The program ends with the value 0
+ */
+
 int main(void)
 {
 
