a57d6abbde7be55385883bdf4fe1e5e3cd528623
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 13:02:26 -0700

    COMMIT TASK 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of the character.
+ * 
+ * @n: An input character
+ * 
+ * Description: function returns 1 if the character is a
+ * letter, lowercase or uppercase.
+ *
+ * Return: 1 or 0 in otherwise.
+ */
+
+int print_sign(int n)
+{
+	int n;
+	char sign;
+	
+	if (n == 0)
+	{
+		sign = 0;
+	}
+	if else (n < 0)
+	{
+		sign = -;
+	}
+	else
+	{
+		sign = +;
+	}
+
+	return (sign);
+}
