5bb22ab46d225cbafc107128d2aa46f911d2fe3d
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 06:52:36 -0700

    COMMIT TASK 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print alphabets in lowercase
+ *
+ * Description: a function that prints the alphabet,
+ * in lowercase, followed by a new line
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
