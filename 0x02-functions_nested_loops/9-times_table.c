433150db4825d94c479f34cf65756255da945fa3
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 16:22:05 -0700

    JUST COMMIT

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - print multiplication table
+ * Description: function that prints all natural numbers
+ * from n to 98, followed by a new line.
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int c;
+	int r;
+	
+	for (c = 0; c < 10; c++)
+	{
+		for (r = 0; r < 10; r++)
+		{
+		printf("%d", c * r, ", ");
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
