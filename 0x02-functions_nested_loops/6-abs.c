d8ac7e563e8f0d37fe3482950f16bffdbc421a98
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 13:55:11 -0700

    COMMIT TASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of the r
+ * @r: An integre input
+ * Return: absolute value
+ * Description: This function returns absolute value of a number
+ */
+
+int _abs(int)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-1 * r);
+	}
+}
