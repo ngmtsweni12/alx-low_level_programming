c8d05ce762771085c2dbd01de3f00e89682edc4c
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 06:16:30 -0700

    commit task 0 but is executeble

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include <stdio.h>;
-#include <stlib.h>;
+#include "main.h"
 
-/*
- * main - Main Entry
+/**
+ * main - print _putchar
  *
- * Description: a program that prints _putchar, followed by a new line
+ * Description: a program that prints _putchar, followed by a new line.
  *
  * Return: 0
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
