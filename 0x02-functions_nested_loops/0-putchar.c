e8b65ecdd68aa9fee73191d80da0d0bfecd0db28
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-13 04:54:08 -0700

    COMMIT TASK 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>;
+#include <stlib.h>;
+
+/*
+ * main - Main Entry
+ *
+ * Description: a program that prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
