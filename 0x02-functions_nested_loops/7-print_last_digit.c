1e41e25c89325c70cd76f68a73f7a2d1ac9e4185
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-14 12:49:49 -0700

    COMMIT TASK 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_last_digit - print the last digit
  * @r: An integer input
@@ -10,10 +12,15 @@ int print_last_digit(int r)
 	int n;
 	
 	if (r < 0)
+	{
 		n = -1 * (r % 10);
+	}
 	else
+	{
 		n = r % 10;
-	
-	_putchar(n % 10);
+	}
+
+	_putchar((n % 10) + '0');
+
 	return (n % 10);
 }
