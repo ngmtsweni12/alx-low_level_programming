c6d3bad7ba14134bb859d12c4f51bd5c52ba445c
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-15 20:34:42 -0700

    VERY VERY SLEEPY AND TIRED

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ void more_numbers(void)
 {
 	int i;
 	int j;
-
+	
 	for (j= 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i > 9)
+			if (i >= 10)
 				_putchar(i / 10 + '0');
 
 			_putchar(i % 10 + '0');
