8dabb812502811080455dfba1395b3281498aa28
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-15 17:50:08 -0700

    COMMIT MAIN AND PUTCHAR

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED 
+
 #include <stdio.h>
 
 int _isupper(int c);
@@ -12,3 +15,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
+#endif
