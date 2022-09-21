d0df7efc1f5f076925ddef131fc2400d4dd0f489
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-21 13:39:08 -0700

    COMMIT TASK 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,10 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character
+ * puts2 - a function that prints
+ * every other character of a string,
+ * starting with the first character,
+ * followed by a new line.
  * @str: given string
  *
  * Return: void
