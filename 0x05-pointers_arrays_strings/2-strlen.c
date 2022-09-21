9adc328fcf31c2553faa6ce940d52c233ede3e47
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-21 08:52:23 -0700

    COMMIT TASK 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - a function wwhich returns 
+ * the length of a string
+ * @s: given string
+ *Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
