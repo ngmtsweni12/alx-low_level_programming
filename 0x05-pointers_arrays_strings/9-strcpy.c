2170d6dff4a91a95f0011fa3b1a794ca7bac2cf2
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-21 18:56:50 -0700

    COMMIT TASK 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-* _strcpy - a function that copies the string pointed to by src,
-* including the terminating null byte (\0),
-* to the buffer pointed to by dest.
-* @dest: destination
-* @src: source
-* Return: the pointer to dest 
-*/
+ * _strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: destination
+ * @src: source
+ *
+ * Return: the pointer to dest 
+ */
 
 char *_strcpy(char *dest, char *src)
 {
