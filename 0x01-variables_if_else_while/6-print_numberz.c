c979b4b94be05980a0a229edc3b8fd34b1745b7f
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-23 13:57:12 -0700

    COMMIT TASK 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
