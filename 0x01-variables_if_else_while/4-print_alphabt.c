e2b5d0d8a8b02fc1cf02c0e11d9268d1a4cbd653
NOMPUMELELO MTSWENI <ngmtsweni12@gmail.com>
2022-09-23 13:41:21 -0700

    COMMIT TASK 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if !(lowecase == 'e' || lowercase == 'q')
+		{
+			putchar(lowercase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
